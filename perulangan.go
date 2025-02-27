a5a3055041a2a6ca62fe5774a69f7a1b366c4bc2
rofiqabdul <rofiqabdul04@gmail.com>
2025-02-28 01:19:10 +0700

    break & continue

--- perulangan.go
+++ perulangan.go
@@ -4,12 +4,18 @@ import "fmt"
 
 func main() {
 	for counter := 1; counter <= 10; counter++ {
+		if counter == 5 {
+			break
+		}
 		fmt.Println("Perulangan ke", counter)
 	}
 
 	names := []string{"Eko", "Kurniawan", "Khannedy"}
 
 	for i := 0; i < len(names); i++ {
+		if i%2 == 0 {
+			continue
+		}
 		fmt.Println(names[i])
 	}
 
