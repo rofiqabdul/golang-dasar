c78f86216a21e35526ee192d4ff9f0400ea49509
rofiqabdul <rofiqabdul04@gmail.com>
2025-02-28 01:36:57 +0700

    retuning multiple values

--- function.go
+++ function.go
@@ -10,8 +10,16 @@ func getHello(name string) string {
 	return "Hello " + name
 }
 
+func getFullName() (string, string) {
+	return "Eko", "Khaneedy"
+}
+
 func main() {
 	sayHello("Eko", "Kurniawan")
 	result := getHello("Eko")
 	fmt.Println(result)
+
+	fmt.Println(getFullName())
+	firstName, _ := getFullName()
+	fmt.Println(firstName)
 }
