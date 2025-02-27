f38693bb8b3d940fa47ac68cf43b43f94ecae90a
rofiqabdul <rofiqabdul04@gmail.com>
2025-02-28 01:32:08 +0700

    function return value

--- function.go
+++ function.go
@@ -6,6 +6,12 @@ func sayHello(firstName string, lastName string) {
 	fmt.Println("Hello", firstName, lastName)
 }
 
+func getHello(name string) string {
+	return "Hello " + name
+}
+
 func main() {
 	sayHello("Eko", "Kurniawan")
+	result := getHello("Eko")
+	fmt.Println(result)
 }
