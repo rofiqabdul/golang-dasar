2d1499c3d6e3b1f38dde89e84d9d3dfa3f4e517a
rofiqabdul <rofiqabdul04@gmail.com>
2025-03-02 01:20:06 +0700

    access modifier

--- helper/helper.go
+++ helper/helper.go
@@ -1,5 +1,12 @@
 package helper
 
+var version = "1.0.0"      // tidak bisa diakses dari luar package
+var Application = "golang" // bisa diakses dari luar package
+
+func sayGoodbye(name string) string { // tidak bisa diakses dari luar package
+	return "Good bye " + name
+}
+
 func SayHello(name string) string {
 	return "Hello " + name
 }
